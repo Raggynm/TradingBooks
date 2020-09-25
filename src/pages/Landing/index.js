a340cef48b7bf81e132f294a0587640f910176af
Grhamm Pabst <48564739+Tsbap@users.noreply.github.com>
2020-09-24 22:50:07 -0300

    First screen

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,36 +1,65 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import styles from './styles';
-
-import googleIcon from '../../assets/google_icon1.png'
-import twitterIcon from '../../assets/twitter_icon1.png'
-import facebookIcon from '../../assets/facebook_icon1.png'
 
 function Landing() {
     return (
         <View style={styles.container}>
             <Text adjustsFontSizeToFit style={styles.logo}>Trading Books</Text>
-            <Text style={styles.line}/>
-        
-            <TouchableOpacity style={styles.button}>
-                <Image source={googleIcon} />
-                <Text style={styles.buttonText}>Entre com Google </Text>
-            </TouchableOpacity>
-                
-             <TouchableOpacity style={styles.button}>
-                <Image source={twitterIcon} />
-                <Text style={styles.buttonText}>Entre com Twitter </Text>
-            </TouchableOpacity>
-                
-            <TouchableOpacity style={styles.button}>
-                <Image source={facebookIcon} />
-                <Text style={styles.buttonText}>Entre com Facebook </Text>
-            </TouchableOpacity>
+            <Text style={styles.line} />
 
-            <Text style={styles.line2}/>        
-        </View> 
-        
+            <View style={styles.containerButtons}>
+                <View style={styles.containerButton}>
+                    <TouchableOpacity style={styles.button}>
+                        <View style={styles.iconView}>
+                            <Icon name="google" size={40} />
+                        </View>
+                        <View style={styles.buttonTextView}>
+                            <Text style={styles.buttonText}>Entre com Google</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.containerButton}>
+                    <TouchableOpacity style={styles.button}>
+                        <View style={styles.iconView}>
+                            <Icon name="twitter" size={40} />
+                        </View>
+                        <View style={styles.buttonTextView}>
+                            <Text style={styles.buttonText}>Entre com Twitter</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.containerButton}>
+                    <TouchableOpacity style={styles.button}>
+                        <View style={styles.iconView}>
+                            <Icon name="facebook" size={40} />
+                        </View>
+                        <View style={styles.buttonTextView}>
+                            <Text style={styles.buttonText}>Entre com Facebook</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+
+
+            <Text style={styles.line2} />
+
+            <View style={styles.bottomContainer}>
+                <TouchableOpacity style={styles.buttonWoutBackground}>
+                    <Text style={styles.textButtonb2}>Cadastre-se</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonWoutBackground}>
+                    <Text style={styles.textButtonb2}>Entre aqui</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+
     );
 }
 
--- src/pages/Landing/styles.js
+++ src/pages/Landing/styles.js
@@ -2,52 +2,103 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 export const Styles = StyleSheet.create({
-    
-    
+
+
     container: {
         flex: 1,
         backgroundColor: '#D11749',
-        alignItems:'center'
+        alignItems: 'center'
     },
 
     logo: {
-        fontFamily:'RedHatDisplay_400Regular',
-        fontSize: 32,
+        fontFamily: 'RedHatDisplay_400Regular',
+        fontSize: 40,
         marginTop: 80,
-        marginBottom:48,
+        marginBottom: 48,
         color: '#fff'
     },
 
     line: {
-        marginBottom:123,
-        paddingLeft:300,
+        marginBottom: 123,
+        paddingLeft: 300,
         borderBottomColor: '#fff',
         borderBottomWidth: 3,
     },
-    
-    button:{
-        marginBottom:62,
-        height: 104,
-        width:'80%',
-        backgroundColor:'#EEEEEE',
-        borderRadius: 72,
-        alignItems: 'center',
-        justifyContent: 'center',
 
+    iconView: {
+        width: "20%",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    buttonTextView: {
+        width: "70%"
+    },
+
+    button: {
+        backgroundColor: '#EEEEEE',
+        borderRadius: 72,
+        flex: 1,
+        padding: 5,
+        height: 50,
+        margin: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '80%',
+        shadowColor: "#ffffff",
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 1,
+
+        elevation: 5,
+    },
+
+    containerButtons: {
+        height: 250
     },
 
     buttonText: {
-        fontFamily:'RedHatDisplay_500Medium',
-        fontSize: 36,
+        fontFamily: 'RedHatDisplay_500Medium',
+        fontSize: 20,
     },
 
     line2: {
-        marginTop:144,
-        marginBottom:60,
-        paddingLeft:500,
+        marginTop: 144,
+        paddingLeft: 300,
         borderBottomColor: '#fff',
         borderBottomWidth: 3,
     },
+
+    buttonWoutBackground: {
+        margin: 60
+    },
+
+    textButtonb2: {
+        fontSize: 16,
+        color: "#FFF"
+    },
+
+    containerButton: {
+        flex: 1,
+        flexDirection: "column",
+        width: "100%",
+        height: 20,
+        alignItems: "center",
+        justifyContent: "center",
+
+    },
+
+    bottomContainer: {
+        flex: 1,
+        flexDirection: "row",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "space-between"
+    }
 
 });
 
